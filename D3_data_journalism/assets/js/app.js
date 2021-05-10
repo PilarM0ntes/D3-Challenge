151494db8cbc403aa3ad1c1aa5e867404fbc692d
Pilar <pilar.montes01@gmail.com>
2021-05-10 13:20:53 -0500

    Axis and circle labels added

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,8 +6,8 @@ let svgHeight = 660;
 let chartMargin = {
   top: 30,
   right: 30,
-  bottom: 60,
-  left: 30
+  bottom: 80,
+  left: 60
 };
 
 // Define dimensions of the chart area
@@ -29,20 +29,25 @@ let chartGroup = svg.append("g")
 // Load the data frm data.csv
 d3.csv("./assets/data/data.csv").then(function(census_data){
 
-    let selectedData = {
-        poverty: census_data.map(item => +item.poverty),
-        healthcare: census_data.map(item => +item.healthcare)    
-    };
+    // let selectedData = {
+    //     poverty: census_data.map(item => +item.poverty),
+    //     healthcare: census_data.map(item => +item.healthcare)    
+    // };
 
-    console.log(selectedData);
+    census_data.forEach(function(data){
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
+
+    console.log(census_data);
     
     // Create Scales
     let xScale = d3.scaleLinear()
-        .domain([0, d3.max(selectedData.poverty)])
+        .domain([8, d3.max(census_data, d => d.poverty)])
         .range([0, chartWidth]);
     let yScale = d3.scaleLinear()
-        .domain([0,d3.max(selectedData.healthcare)])
-        .range([chartHeight,0]);
+        .domain([0,d3.max(census_data, d => d.healthcare)])
+        .range([chartHeight, 0]);
 
     // Create axes
     let xAxis = d3.axisBottom(xScale);
@@ -52,20 +57,53 @@ d3.csv("./assets/data/data.csv").then(function(census_data){
     // & create the bottom and left axis
 
     chartGroup.append("g")
-        .call(yAxis);
-    chartGroup.append("g")
-        .attr("transform", `translate(0, ${chartHeight})`)
-        .call(xAxis);
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(xAxis);
     
-    chartGroup.selectAll("circle")
-        .data(selectedData)
+    chartGroup.append("g")
+      .call(yAxis);
+    
+    let circleGroup = chartGroup.selectAll(".stateCircle")
+        .data(census_data)
         .enter()
         .append("circle")
-        // .attr("class", "stateCircle")
+        .attr("class", "stateCircle")
         .attr("cx", d => xScale(d.poverty))
         .attr("cy", d => yScale(d.healthcare))
-        .attr("r", "5")
-        .style("fill", "#406278")
+        .attr("r", "20")
+
+    console.log(chartGroup.selectAll("text"));
+    
+    // Create circle labels
+    chartGroup.selectAll(".stateText")
+      .data(census_data)
+      .enter()
+      .append("text")
+      .attr("class", "stateText")
+      .attr("x", d => xScale(d.poverty))
+      .attr("y", d => yScale(d.healthcare)+3)
+      .text(d => d.abbr)
+
+      console.log(d3.selectAll(".stateText"));
+    
+      // Create axes labels
+      chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - chartMargin.left)
+      .attr("x", 0 - (svgHeight / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+      chartGroup.append("text")
+      .attr("transform", `translate(${svgWidth / 2}, ${svgHeight-50})`)
+      .attr("y", 0)
+      .attr("x", -50)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
+
+    
+
     
 
 
